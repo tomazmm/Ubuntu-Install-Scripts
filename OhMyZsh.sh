fe1629c6af7fda9c8638854520224428ff3de788
tomazmm <tomaz.mesarec@gmail.com>
2020-03-17 10:10:15 +0100

    Updated commands

--- OhMyZsh.sh
+++ OhMyZsh.sh
@@ -4,11 +4,11 @@
 
 
 
-apt-get install zsh -y
-apt-get install git-core -y
+sudo apt-get install zsh -y
+sudo apt-get install git-core -y
 
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
-#chsh -s `which zsh`
+chsh -s `which zsh`
 
 #sudo shutdown -r 0
--- spotify.sh
+++ spotify.sh
@@ -4,15 +4,15 @@
 # Install spotify
 ###############################################
 # 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 
-# 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# # 2. Add the Spotify repository
+# echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
-# 3. Update list of available packages
-sudo apt-get update
+# # 3. Update list of available packages
+# sudo apt-get update
 
-# 4. Install Spotify
-sudo apt-get install spotify-client
+# # 4. Install Spotify
+# sudo apt-get install spotify-client
 
-
+sudo snap install spotify
